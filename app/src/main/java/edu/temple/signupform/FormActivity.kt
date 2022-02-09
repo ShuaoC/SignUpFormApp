4aa326ba8253c40b0af83f0650128deec5493510
ShuaoC <felix19981228@gmail.com>
2022-02-09 01:03:16 -0500

    Implemented multiple if statements to check different errors, as well as implemented toast

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
@@ -12,9 +14,39 @@ class FormActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val nameText = findViewById<EditText>(R.id.UserName)
+        val name = findViewById<EditText>(R.id.UserName)
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val cpassword = findViewById<EditText>(R.id.confirmPassword)
+
         findViewById<Button>(R.id.save).setOnClickListener{
-            println(nameText.text.toString())
+            if(name.text.toString()==""){
+                name?.error = "Name cannot be empty"
+            }
+            if(email.text.toString()==""){
+                email?.error = "Email cannot be empty"
+            }
+            if(password.text.toString()==""){
+                password?.error = "Password cannot be empty"
+            }
+            if(cpassword.text.toString()==""){
+                cpassword?.error = "Password cannot be empty"
+            }
+            if(cpassword.text.toString()!=password.text.toString()){
+                cpassword?.error = "Password must be the same"
+            }
+
+            toast(name.text.toString())
+
         }
     }
+
+    fun toast(name: String){
+        val text = "Welcome, " + name + ", to the SignUpForm App"
+        val duration = Toast.LENGTH_LONG
+
+        val toast = Toast.makeText(applicationContext, text, duration)
+        toast.show()
+    }
+
 }
