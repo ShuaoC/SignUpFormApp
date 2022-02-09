4434b243761fda65da019f3a201777969080d610
ShuaoC <felix19981228@gmail.com>
2022-02-09 00:39:35 -0500

    Changed from TextInputEditText to EditText

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -2,10 +2,19 @@ package edu.temple.signupform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class FormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nameText = findViewById<EditText>(R.id.UserName)
+        findViewById<Button>(R.id.save).setOnClickListener{
+            println(nameText.text.toString())
+        }
     }
 }
